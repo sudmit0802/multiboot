cb3c5d577f37279e5059b48c38c3d7f27983fa95
sudmit0802 <sudakov.di@edu.spbstu.ru>
2024-03-04 11:32:19 +0300

    + tetris submodule fix 2

---
+++ tetris/src/init.c
@@ -0,0 +1,27 @@
+#include "init.h"
+#include "tetris.h"
+
+void init() {
+
+    if(TTF_Init() == -1) {
+        fprintf(stderr,
+                "\nTTF_Init Error:  %s\n",
+                SDL_GetError());
+        exit(1);
+    }
+
+    init_graphics();
+
+    initTetris();
+
+}
+
+void cleanup() {
+
+    cleanup_graphics();
+
+    TTF_Quit();
+
+    // Shut down SDL
+    SDL_Quit();
+}
---
+++ tetris/src/init.h
@@ -0,0 +1,11 @@
+#ifndef INIT_H
+#define INIT_H
+
+#include "defs.h"
+#include "graphics.h"
+
+
+void init();
+void cleanup();
+
+#endif
